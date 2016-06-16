372a8723dc1cc05e75eb272d73d9a0c819630996
domenvajevec <domenvajevec@gmail.com>
2016-06-16 14:12:34 -0700

    Initial component in ES5

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-document.write('Hello Frontend Masters!')
+var React = require('react');
+
+var Hello = React.createClass({
+  render: function() {
+    return (
+      <div>Hello, {this.props.name}</div>
+    );
+  }
+});
+
+React.render(<Hello name="Duderino" />, document.body)
