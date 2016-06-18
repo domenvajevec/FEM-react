03cf48d8f47a28946d86223cb784d233e521cf5b
domenvajevec <domenvajevec@gmail.com>
2016-06-17 17:19:48 -0700

    Setup public and repos page

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import styles from './styles/main.css'
+import Router from './router'
+import styles from './styles/main.styl'
 
-const Hello = React.createClass({
-  render() {
-    return <div>Hello, {this.props.name}!</div>
+window.app = {
+  init() {
+    this.router = new Router()
+    this.router.history.start()
   }
-})
+}
 
-React.render(<Hello name="Duderino" />, document.body)
+window.app.init()
